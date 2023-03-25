a7c5c30bfaf198f362a6bcbd8319581d7c22f227
Christian Fenn <cfenn2020@fau.edu>
2023-03-24 22:30:26 -0400

    Completed Hero Section/Added 3D Model

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
+const CanvasLoader = () => {
+  // Shows loading progress of canvas
+  const { progress } = useProgress();
+
   return (
-    <div>Loader</div>
-  )
-}
+    // Using Html from three.js since three.js does not allow divs
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
 
-export default Loader
+export default CanvasLoader;
