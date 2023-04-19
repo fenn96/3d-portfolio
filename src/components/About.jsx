6c80496dd26b4087465c98ea88eb9478c35c7d21
Christian Fenn <cfenn2020@fau.edu>
2023-04-19 13:18:13 -0400

    Fixed typo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         I'm a skilled software developer with experience in TypeScript and JavaScript,
-        and experise in frameworks like React and Node.js. I'm a quick learner and collaborate well
+        and expertise in frameworks like React and Node.js. I'm a quick learner and collaborate well
         with teams of other developers to create efficient, scalable and user-friendly solutions 
         that solve real-world problems. Let's work together to bring your ideas to life! 
       </motion.p>
